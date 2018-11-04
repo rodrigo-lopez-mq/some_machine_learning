29019ae3aa9937a950bdd9ef511a213962841a52
unknown <sibneq@gmail.com>
2018-11-03 23:34:28 -0400

    Added image classification project

---
+++ MLExample/extensorflow/imageClassification/cnn_ex1/ex1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 22:55:42 2018
+
+@author: r
+"""
+
+#https://pythonprogramming.net/cnn-tensorflow-convolutional-nerual-network-machine-learning-tutorial/
+#https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d
